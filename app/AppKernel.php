586060e94c33179d63d0008deb4363b40500dad1
Morten Meilby <mm@iserve.dk>
2014-11-30 15:18:13 +0100

    Support for location in test environment

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -61,4 +61,11 @@ class AppKernel extends Kernel
     {
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
+    
+    public function init() {
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            date_default_timezone_set( 'Europe/Copenhagen' );
+        }
+        parent::init();
+    }
 }

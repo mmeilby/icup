4b8337f526f2b4d7a5ff6773e6ec5e3fe4796d22
Morten Meilby <mm@iserve.dk>
2015-04-08 23:08:46 +0200

    Frontpage brush up Champions page brush up

--- src/ICup/Bundle/PublicSiteBundle/Controller/Tournament/OverviewController.php
+++ src/ICup/Bundle/PublicSiteBundle/Controller/Tournament/OverviewController.php
@@ -4,6 +4,9 @@ namespace ICup\Bundle\PublicSiteBundle\Controller\Tournament;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use ICup\Bundle\PublicSiteBundle\Controller\User\SelectClubController;
+use Symfony\Component\HttpFoundation\Request;
+use DateTime;
 
 class OverviewController extends Controller
 {
@@ -11,13 +14,35 @@ class OverviewController extends Controller
      * @Route("/tmnt/vw/{tournament}", name="_tournament_overview")
      * @Template("ICupPublicSiteBundle:Tournament:overview.html.twig")
      */
-    public function overviewAction($tournament)
+    public function overviewAction(Request $request, $tournament)
     {
         $this->get('util')->setTournamentKey($tournament);
         $tournament = $this->get('util')->getTournament();
         if ($tournament == null) {
             return $this->redirect($this->generateUrl('_tournament_select'));
         }
+        
+        $club_list = $this->getClubList($request);
+        $today = new DateTime();
+//        $today = new DateTime('2014-07-06 20:15:00');
+
+        if (count($club_list) > 0) {
+            $matchList = $this->get('match')->listMatchesByTournament($tournament->getId(), $club_list);
+        }
+        else {
+            $matchList = $this->get('match')->listMatchesLimitedWithTournament($tournament->getId(), $today, 10, 6);
+        }
+        $matches = array();
+        foreach ($matchList as $match) {
+            $matches[date_format($match['schedule'], "Y/m/d")][] = $match;
+        }
+        
+        $shortMatchList = $matchList;
+        $shortMatches = array();
+        foreach ($shortMatchList as $match) {
+            $shortMatches[date_format($match['schedule'], "Y/m/d")][] = $match;
+        }
+        
         $newsStream = array(
 /*            
             array(
@@ -62,6 +87,28 @@ class OverviewController extends Controller
 
         return array('tournament' => $tournament,
                      'newsstream' => $newsStream,
+                     'matchlist' => $shortMatches,
                      'teaserlist' => $teaserList);
     }
+    
+    public function getClubList(Request $request) {
+        $clubs = array();
+        $club_list = $request->cookies->get(SelectClubController::$ENV_CLUB_LIST, '');
+        foreach (explode(':', $club_list) as $club_ident) {
+            $club_ident_array = explode('|', $club_ident);
+            $name = $club_ident_array[0];
+            if (count($club_ident_array) > 1) {
+                $countryCode = $club_ident_array[1];
+            }
+            else {
+                $countryCode = 'EUR';
+            }
+            $club = $this->get('logic')->getClubByName($name, $countryCode);
+            if ($club) {
+                $clubs[] = $club->getId();
+            }
+        }
+        return $clubs;
+    }
+    
 }

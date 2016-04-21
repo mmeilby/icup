3f55bef5484e9ded82ae54ec061b77552c3c1916
Morten Meilby <mm@iserve.dk>
2016-04-21 18:03:23 +0200

    Group planning with drag and drop

--- src/ICup/Bundle/PublicSiteBundle/Controller/Rest/Tournament/RestCategoryController.php
+++ src/ICup/Bundle/PublicSiteBundle/Controller/Rest/Tournament/RestCategoryController.php
@@ -1,23 +1,248 @@
 <?php
+
 namespace ICup\Bundle\PublicSiteBundle\Controller\Rest\Tournament;
 
-use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\Category;
 use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\Group;
 use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\Tournament;
+use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\User;
 use ICup\Bundle\PublicSiteBundle\Entity\TeamInfo;
+use ICup\Bundle\PublicSiteBundle\Exceptions\ValidationException;
 use ICup\Bundle\PublicSiteBundle\Services\Util;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\Category;
+use ICup\Bundle\PublicSiteBundle\Form\Doctrine\CategoryType;
 
+/**
+ * Doctrine\Category controller.
+ *
+ * @Route("/rest/category")
+ */
 class RestCategoryController extends Controller
 {
     /**
+     * List all the categories identified by tournament id
+     * @Route("/list/{tournamentid}", name="_rest_list_categories", options={"expose"=true})
+     * @Method("GET")
+     * @param $tournamentid
+     * @return JsonResponse
+     */
+    public function indexAction($tournamentid)
+    {
+        /* @var $tournament Tournament */
+        try {
+            $tournament = $this->get('entity')->getTournamentById($tournamentid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        return new JsonResponse($tournament->getCategories()->toArray());
+    }
+
+    /**
+     * Finds and displays a Doctrine\Category entity.
+     *
+     * @Route("/{categoryid}", name="rest_get_category", options={"expose"=true})
+     * @Method("GET")
+     * @param $categoryid
+     * @return JsonResponse
+     */
+    public function showAction($categoryid)
+    {
+        /* @var $category Category */
+        try {
+            $category = $this->get('entity')->getCategoryById($categoryid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        return new JsonResponse(array("category" => $category));
+    }
+
+    /**
+     * Creates a new Doctrine\Category entity.
+     *
+     * @Route("/{tournamentid}", name="rest_category_create", options={"expose"=true})
+     * @Method("POST")
+     * @param Request $request
+     * @param $tournamentid
+     * @return JsonResponse
+     */
+    public function newAction(Request $request, $tournamentid)
+    {
+        /* @var $utilService Util */
+        $utilService = $this->get('util');
+        /* @var $user User */
+        $user = $utilService->getCurrentUser();
+        /* @var $tournament Tournament */
+        try {
+            $tournament = $this->get('entity')->getTournamentById($tournamentid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        try {
+            $host = $tournament->getHost();
+            $utilService->validateEditorAdminUser($user, $host);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_FORBIDDEN);
+        }
+
+        /* @var $category Category */
+        $category = new Category();
+        $category->setTournament($tournament);
+        $form = $this->createForm(new CategoryType(), $category);
+        $form->handleRequest($request);
+        if ($this->checkForm($form, $category)) {
+            $tournament->getCategories()->add($category);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+            return new JsonResponse(array("id" => $category->getId()), Response::HTTP_CREATED);
+        }
+
+        $errors = array();
+        foreach ($form->getErrors(true) as $error) {
+            $errors[] = $error->getMessage();
+        }
+        return new JsonResponse(array('errors' => $errors), Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Edits an existing Doctrine\Category entity.
+     *
+     * @Route("/{categoryid}", name="rest_category_update", options={"expose"=true})
+     * @Method("PUT")
+     * @param Request $request
+     * @param $categoryid
+     * @return JsonResponse
+     */
+    public function updateAction(Request $request, $categoryid)
+    {
+        /* @var $utilService Util */
+        $utilService = $this->get('util');
+        /* @var $user User */
+        $user = $utilService->getCurrentUser();
+        /* @var $category Category */
+        try {
+            $category = $this->get('entity')->getCategoryById($categoryid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        try {
+            $host = $category->getTournament()->getHost();
+            $utilService->validateEditorAdminUser($user, $host);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_FORBIDDEN);
+        }
+
+        $form = $this->createForm(new CategoryType(), $category, array('method' => 'PUT'));
+        $form->handleRequest($request);
+
+        if ($this->checkForm($form, $category)) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return new JsonResponse(array(), Response::HTTP_NO_CONTENT);
+        }
+
+        $errors = array();
+        foreach ($form->getErrors(true) as $error) {
+            $errors[] = $error->getMessage();
+        }
+        return new JsonResponse(array('errors' => $errors), Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Deletes a Doctrine\Category entity.
+     *
+     * @Route("/{categoryid}", name="rest_category_delete", options={"expose"=true})
+     * @Method("DELETE")
+     * @param $categoryid
+     * @return JsonResponse
+     */
+    public function deleteAction($categoryid)
+    {
+        /* @var $utilService Util */
+        $utilService = $this->get('util');
+        /* @var $user User */
+        $user = $utilService->getCurrentUser();
+        /* @var $category Category */
+        try {
+            $category = $this->get('entity')->getCategoryById($categoryid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        try {
+            $host = $category->getTournament()->getHost();
+            $utilService->validateEditorAdminUser($user, $host);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_FORBIDDEN);
+        }
+
+        $errors = array();
+        if ($category->getGroups()->count() > 0) {
+            $errors[] = $this->get('translator')->trans('FORM.CATEGORY.GROUPSEXIST', array(), 'admin');
+        }
+        if ($category->getEnrollments()->count() > 0) {
+            $errors[] = $this->get('translator')->trans('FORM.CATEGORY.ENROLLEDEXIST', array(), 'admin');
+        }
+        if ($category->getPlaygroundattributes()->count() > 0) {
+            $errors[] = $this->get('translator')->trans('FORM.CATEGORY.PARELATIONSEXIST', array(), 'admin');
+        }
+        if (count($errors) == 0) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($category);
+            $em->flush();
+            return new JsonResponse(array(), Response::HTTP_NO_CONTENT);
+        }
+        return new JsonResponse(array('errors' => $errors), Response::HTTP_BAD_REQUEST);
+    }
+
+    private function checkForm(Form $form, Category $category) {
+        if ($form->isValid()) {
+            if ($category->getName() == null || trim($category->getName()) == '') {
+                $form->addError(new FormError($this->get('translator')->trans('FORM.CATEGORY.NONAME', array(), 'admin')));
+            }
+            else {
+                $em = $this->getDoctrine()->getManager();
+                $othercategory = $em->getRepository($form->getConfig()->getOption("data_class"))->findBy(array('tournament' => $category->getTournament()->getId(), 'name' => $category->getName()));
+                if ($othercategory) {
+                    $form->addError(new FormError($this->get('translator')->trans('FORM.CATEGORY.NAMEEXISTS', array(), 'admin')));
+                }
+            }
+            if ($category->getGender() == null) {
+                $form->addError(new FormError($this->get('translator')->trans('FORM.CATEGORY.NOGENDER', array(), 'admin')));
+            }
+            if ($category->getClassification() == null) {
+                $form->addError(new FormError($this->get('translator')->trans('FORM.CATEGORY.NOCLASSIFICATION', array(), 'admin')));
+            }
+            if ($category->getAge() == null || trim($category->getAge()) == '') {
+                $form->addError(new FormError($this->get('translator')->trans('FORM.CATEGORY.NOAGE', array(), 'admin')));
+            }
+            if ($category->getMatchtime() == null || trim($category->getMatchtime()) == '') {
+                $form->addError(new FormError($this->get('translator')->trans('FORM.CATEGORY.NOMATCHTIME', array(), 'admin')));
+            }
+        }
+        return $form->isValid();
+    }
+
+    /**
      * List the clubs by groups assigned in the category
-     * @Route("/rest/category/list/assigned/{categoryid}", name="_rest_list_groups_with_teams", options={"expose"=true})
+     * @Route("/list/assigned/{categoryid}", name="_rest_list_groups_with_teams", options={"expose"=true})
+     * @param $categoryid
+     * @return JsonResponse
      */
     public function restListTeamsByGroupAction($categoryid)
     {
@@ -53,3 +278,4 @@ class RestCategoryController extends Controller
         return new JsonResponse(array('groups' => $groups, 'unassigned' => array('teams' => $teamsUnassigned)));
     }
 }
+
--- src/ICup/Bundle/PublicSiteBundle/Controller/Rest/Tournament/RestGroupController.php
+++ src/ICup/Bundle/PublicSiteBundle/Controller/Rest/Tournament/RestGroupController.php
@@ -1,25 +1,227 @@
 <?php
+
 namespace ICup\Bundle\PublicSiteBundle\Controller\Rest\Tournament;
 
-use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\Category;
 use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\Group;
-use ICup\Bundle\PublicSiteBundle\Entity\TeamInfo;
+use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\User;
+use ICup\Bundle\PublicSiteBundle\Exceptions\ValidationException;
+use ICup\Bundle\PublicSiteBundle\Form\Doctrine\GroupType;
 use ICup\Bundle\PublicSiteBundle\Services\Util;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use ICup\Bundle\PublicSiteBundle\Entity\Doctrine\Category;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
+/**
+ * Doctrine\Group controller.
+ *
+ * @Route("/rest/group")
+ */
 class RestGroupController extends Controller
 {
     /**
-     * Get the playground identified by playground id
-     * @Route("/rest/group/list/{categoryid}", name="_rest_list_groups", options={"expose"=true})
+     * List all the groups identified by category id
+     * @Route("/list/{categoryid}", name="_rest_list_groups", options={"expose"=true})
+     * @Method("GET")
+     * @param $categoryid
+     * @return JsonResponse
      */
-    public function restListGroupAction($categoryid)
+    public function indexAction($categoryid)
     {
         /* @var $category Category */
-        $category = $this->get('entity')->getCategoryById($categoryid);
-        $groups = $category->getGroupsClassified(Group::$PRE);
-        return new Response(json_encode($groups));
+        try {
+            $category = $this->get('entity')->getCategoryById($categoryid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        return new JsonResponse($category->getGroupsClassified(Group::$PRE)->toArray());
+    }
+
+    /**
+     * Finds and displays a Doctrine\Group entity.
+     *
+     * @Route("/{groupid}", name="rest_get_group", options={"expose"=true})
+     * @Method("GET")
+     * @param $groupid
+     * @return JsonResponse
+     */
+    public function showAction($groupid)
+    {
+        /* @var $group Group */
+        try {
+            $group = $this->get('entity')->getGroupById($groupid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        return new JsonResponse(array("group" => $group));
+    }
+
+    /**
+     * Creates a new Doctrine\Group entity.
+     *
+     * @Route("/{categoryid}", name="rest_group_create", options={"expose"=true})
+     * @Method("POST")
+     * @param Request $request
+     * @param $categoryid
+     * @return JsonResponse
+     */
+    public function newAction(Request $request, $categoryid)
+    {
+        /* @var $utilService Util */
+        $utilService = $this->get('util');
+        /* @var $user User */
+        $user = $utilService->getCurrentUser();
+        /* @var $category Category */
+        try {
+            $category = $this->get('entity')->getCategoryById($categoryid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        try {
+            $host = $category->getTournament()->getHost();
+            $utilService->validateEditorAdminUser($user, $host);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_FORBIDDEN);
+        }
+
+        /* @var $group Group */
+        $group = new Group();
+        $group->setCategory($category);
+        $form = $this->createForm(new GroupType(), $group);
+        $form->handleRequest($request);
+        if ($this->checkForm($form, $group)) {
+            $category->getGroups()->add($group);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($group);
+            $em->flush();
+            return new JsonResponse(array("id" => $group->getId()), Response::HTTP_CREATED);
+        }
+
+        $errors = array();
+        foreach ($form->getErrors(true) as $error) {
+            $errors[] = $error->getMessage();
+        }
+        return new JsonResponse(array('errors' => $errors), Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Edits an existing Doctrine\Group entity.
+     *
+     * @Route("/{groupid}", name="rest_group_update", options={"expose"=true})
+     * @Method("PUT")
+     * @param Request $request
+     * @param $groupid
+     * @return JsonResponse
+     */
+    public function updateAction(Request $request, $groupid)
+    {
+        /* @var $utilService Util */
+        $utilService = $this->get('util');
+        /* @var $user User */
+        $user = $utilService->getCurrentUser();
+        /* @var $group Group */
+        try {
+            $group = $this->get('entity')->getGroupById($groupid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        try {
+            $host = $group->getCategory()->getTournament()->getHost();
+            $utilService->validateEditorAdminUser($user, $host);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_FORBIDDEN);
+        }
+        
+        $form = $this->createForm(new GroupType(), $group, array('method' => 'PUT'));
+        $form->handleRequest($request);
+
+        if ($this->checkForm($form, $group)) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+            return new JsonResponse(array(), Response::HTTP_NO_CONTENT);
+        }
+
+        $errors = array();
+        foreach ($form->getErrors(true) as $error) {
+            $errors[] = $error->getMessage();
+        }
+        return new JsonResponse(array('errors' => $errors), Response::HTTP_BAD_REQUEST);
+    }
+
+    /**
+     * Deletes a Doctrine\Group entity.
+     *
+     * @Route("/{groupid}", name="rest_group_delete", options={"expose"=true})
+     * @Method("DELETE")
+     * @param $groupid
+     * @return JsonResponse
+     */
+    public function deleteAction($groupid)
+    {
+        /* @var $utilService Util */
+        $utilService = $this->get('util');
+        /* @var $user User */
+        $user = $utilService->getCurrentUser();
+        /* @var $group Group */
+        try {
+            $group = $this->get('entity')->getGroupById($groupid);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_NOT_FOUND);
+        }
+        try {
+            $host = $group->getCategory()->getTournament()->getHost();
+            $utilService->validateEditorAdminUser($user, $host);
+        }
+        catch (ValidationException $e) {
+            return new JsonResponse(array('errors' => array($e->getMessage())), Response::HTTP_FORBIDDEN);
+        }
+
+        $errors = array();
+        if ($group->getGroupOrder()->count() > 0) {
+            $errors[] = $this->get('translator')->trans('FORM.GROUP.ORDEREXIST', array(), 'admin');
+        }
+        if ($group->getMatches()->count() > 0) {
+            $errors[] = $this->get('translator')->trans('FORM.GROUP.MATCHESEXIST', array(), 'admin');
+        }
+        if (count($errors) == 0) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($group);
+            $em->flush();
+            return new JsonResponse(array(), Response::HTTP_NO_CONTENT);
+        }
+        return new JsonResponse(array('errors' => $errors), Response::HTTP_BAD_REQUEST);
+    }
+
+    private function checkForm(Form $form, Group $group) {
+        if ($form->isValid()) {
+            if ($group->getName() == null || trim($group->getName()) == '') {
+                $form->addError(new FormError($this->get('translator')->trans('FORM.GROUP.NONAME', array(), 'admin')));
+            }
+            else {
+                $em = $this->getDoctrine()->getManager();
+                $othergroup = $em->getRepository($form->getConfig()->getOption("data_class"))->findBy(array('category' => $group->getCategory()->getId(), 'name' => $group->getName()));
+                if ($othergroup) {
+                    $form->addError(new FormError($this->get('translator')->trans('FORM.GROUP.NAMEEXISTS', array(), 'admin')));
+                }
+            }
+            if ($group->getClassification() === null) {
+                $form->addError(new FormError($this->get('translator')->trans('FORM.GROUP.NOCLASSIFICATION', array(), 'admin')));
+            }
+        }
+        return $form->isValid();
     }
 }

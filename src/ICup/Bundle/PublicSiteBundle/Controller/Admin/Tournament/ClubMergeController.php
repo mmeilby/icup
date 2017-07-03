a39656228dd8affd612f1b7ae2a2a782f423a12f
Morten Meilby <mm@iserve.dk>
2017-07-03 16:26:00 +0200

    Bug fix - merge clubs

--- src/ICup/Bundle/PublicSiteBundle/Controller/Admin/Tournament/ClubMergeController.php
+++ src/ICup/Bundle/PublicSiteBundle/Controller/Admin/Tournament/ClubMergeController.php
@@ -12,6 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use ICup\Bundle\PublicSiteBundle\Exceptions\ValidationException;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
+use Exception;
 
 class ClubMergeController extends Controller
 {
@@ -55,53 +56,61 @@ class ClubMergeController extends Controller
     }
     
     private function mergeClubs(Club $source_club, Club $target_club) {
-        $em = $this->getDoctrine()->getManager();
-        $teams = $source_club->getTeams();
-        /* @var $category Category */
-        foreach ($teams as $team) {
-            /* @var $team Team */
-            try {
-                $category = $team->getCategory();
+        $em = $this->getDoctrine()->getEntityManager();
+        $em->beginTransaction();
+        try {
+            $teams = $source_club->getTeams();
+            /* @var $category Category */
+            foreach ($teams as $team) {
+                /* @var $team Team */
+                try {
+                    $category = $team->getCategory();
+                }
+                catch (ValidationException $ex) {
+                    $category = $this->get('logic')->getAssignedCategory($team->getId());
+                }
+                $enrolledteams = $this->get('logic')->listEnrolledTeamsByCategory($category->getId(), $target_club->getId());
+                $noTeams = count($enrolledteams);
+                if ($noTeams >= 26) {
+                    // Can not add more than 26 teams to same category - Team A -> Team Z - make an exception here...
+                    $division = 'm'.$team->getDivision();
+                }
+                else if ($noTeams == 0) {
+                    $division = '';
+                }
+                else if ($noTeams == 1) {
+                    $division = 'B';
+                    $firstteam = array_shift($enrolledteams);
+                    $firstteam->setDivision('A');
+                }
+                else {
+                    $division = chr($noTeams + 65);
+                }
+                $team->setClub($target_club);
+                $team->setName($target_club->getName());
+                $team->setDivision($division);
+                $em->flush();
             }
-            catch (ValidationException $ex) {
-                $category = $this->get('logic')->getAssignedCategory($team->getId());
+            foreach ($source_club->getOfficials() as $official) {
+                /* @var $official ClubRelation */
+                $target_club->getOfficials()->add($official);
+                $official->setClub($target_club);
             }
-            $enrolledteams = $this->get('logic')->listEnrolledTeamsByCategory($category->getId(), $target_club->getId());
-            $noTeams = count($enrolledteams);
-            if ($noTeams >= 26) {
-                // Can not add more than 26 teams to same category - Team A -> Team Z - make an exception here...
-                $division = 'm'.$team->getDivision();
+            foreach ($source_club->getVouchers() as $voucher) {
+                /* @var $voucher Voucher */
+                $target_club->getVouchers()->add($voucher);
+                $voucher->setClub($target_club);
             }
-            else if ($noTeams == 0) {
-                $division = '';
-            }
-            else if ($noTeams == 1) {
-                $division = 'B';
-                $firstteam = array_shift($enrolledteams);
-                $firstteam->setDivision('A');
-            }
-            else {
-                $division = chr($noTeams + 65);
-            }
-            $team->setClub($target_club);
-            $team->setName($target_club->getName());
-            $team->setDivision($division);
+            $source_club->getTeams()->clear();
+            $source_club->getOfficials()->clear();
+            $source_club->getVouchers()->clear();
+            $em->remove($source_club);
             $em->flush();
+            $em->commit();
         }
-        foreach ($source_club->getOfficials() as $official) {
-            /* @var $official ClubRelation */
-            $target_club->getOfficials()->add($official);
-            $official->setClub($target_club);
+        catch (Exception $e) {
+            $em->rollBack();
+            throw $e;
         }
-        foreach ($source_club->getVouchers() as $voucher) {
-            /* @var $voucher Voucher */
-            $target_club->getVouchers()->add($voucher);
-            $voucher->setClub($target_club);
-        }
-        $source_club->getTeams()->clear();
-        $source_club->getOfficials()->clear();
-        $source_club->getVouchers()->clear();
-        $em->remove($source_club);
-        $em->flush();
     }
 }

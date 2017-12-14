6e95209ab5e0c78726ef41aeef891ccb24adc5fb
Morten Meilby <mm@iserve.dk>
2017-12-14 08:09:22 +0100

    Downgrade to php 5.4

---
+++ src/ICup/Bundle/PublicSiteBundle/Form/Doctrine/MatchType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace ICup\Bundle\PublicSiteBundle\Form\Doctrine;
+
+use ICup\Bundle\PublicSiteBundle\Entity\MatchForm;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MatchType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('matchno')
+            ->add('group')
+            ->add('playground')
+            ->add('date')
+            ->add('time')
+        ;
+    }
+
+    /**
+     * Configures the options for this type.
+     *
+     * @param OptionsResolver $resolver The resolver for the options.
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => MatchForm::class,
+            'csrf_protection' => false,
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return '';
+    }
+}
